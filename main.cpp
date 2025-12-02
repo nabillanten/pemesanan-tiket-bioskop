eead756c87121888d80758f8da699b19f41b8132
Nabil Lanten <nabilanten3@gmail.com>
2025-12-02 16:40:10 +0700

    feat: add, insert, find, print film

--- main.cpp
+++ main.cpp
@@ -30,6 +30,83 @@ struct Film
     Pemesanan *next_pemesanan;
 };
 
+// Menambah Film (Parent)
+Film *addFilm(string judul_film, string tanggal_tayang, string jam_tayang, string studio)
+{
+
+    static int counter = 1;
+
+    Film *new_film = new Film();
+
+    new_film->id_film = "F-" + string(3 - to_string(counter).length(), '0') + to_string(counter);
+    new_film->judul_film = judul_film;
+    new_film->tanggal_tayang = tanggal_tayang;
+    new_film->jam_tayang = jam_tayang;
+    new_film->studio = studio;
+    new_film->next_film = new_film->prev_film = NULL;
+    new_film->next_pemesanan = NULL;
+
+    counter++;
+
+    return new_film;
+}
+
+// Insert Last Film (Parent)
+void insertFilm(string judul_film, string tanggal_tayang, string jam_tayang, string studio, Film *&head, Film *&tail)
+{
+    Film *new_film = addFilm(judul_film, tanggal_tayang, jam_tayang, studio);
+
+    if (tail == NULL)
+    {
+        head = tail = new_film;
+    }
+    else
+    {
+        tail->next_film = new_film;
+        new_film->prev_film = tail;
+        tail = new_film;
+    }
+}
+
+// Cari Film (Parent)
+Film *findFilm(string judul, Film *&head)
+{
+    Film *p = head;
+
+    while (p != NULL)
+    {
+        if (p->judul_film == judul)
+            return p;
+        p = p->next_film;
+    }
+    return NULL;
+}
+
+// Menampilkan List Film (Parent)
+void printListFilm(Film *head)
+{
+    Film *temp = head;
+
+    cout << "LIST FILM : " << endl
+         << endl;
+
+    while (temp != NULL)
+    {
+        cout << "ID Film : " << temp->id_film << endl;
+        cout << "Judul Film : " << temp->judul_film << endl;
+        cout << "Tanggal Tayang : " << temp->tanggal_tayang << endl;
+        cout << "Jam Tayang : " << temp->jam_tayang << endl;
+        cout << "Studio : " << temp->studio << endl;
+        if (temp->next_film != NULL) // Memeriksa apakah ada node berikutnya.
+            cout << endl;
+        temp = temp->next_film;
+    }
+    cout << endl;
+    cout << "===============================================";
+    cout << endl
+         << endl;
+}
+
 int main()
 {
 
@@ -39,6 +116,12 @@ int main()
 
     // Singly Linked List Penonton (Child)
     Penonton *headPenonton = NULL;
+
+    // Insert Film ke Doubly Linked List (Parent)
+    insertFilm("Harry Potter", "02-12-2025", "20:30", "STD-01", firstFilm, lastFilm);
+    insertFilm("Harry Potter II", "03-12-2025", "10:30", "STD-01", firstFilm, lastFilm);
+
+    printListFilm(firstFilm);
 
     return 0;
 }
